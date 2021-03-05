a05fe16c39379f11840cc9a00b547776c41f2eaf
sqdog <joshua.grass@oculus.com>
2021-03-05 13:40:19 -0800

    touch fix 5

--- games/REclaimer/webapp/service-worker.js
+++ games/REclaimer/webapp/service-worker.js
@@ -1,5 +1,5 @@
 // Service-work cache name
-var cacheName = '2021_03_05__13_33:static';
+var cacheName = '2021_03_05__13_37:static';
 
 // Cache assets on install
 self.addEventListener('install', function(e) {
