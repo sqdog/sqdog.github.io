e914397a048869de4ba919077c08aaa6659a5327
sqdog <joshua.grass@oculus.com>
2021-03-06 09:38:38 -0800

    ipad perf

--- games/REclaimer/webapp/service-worker.js
+++ games/REclaimer/webapp/service-worker.js
@@ -1,5 +1,5 @@
 // Service-work cache name
-var cacheName = '2021_03_06__09_19:static';
+var cacheName = '2021_03_06__09_38:static';
 
 // Cache assets on install
 self.addEventListener('install', function(e) {
