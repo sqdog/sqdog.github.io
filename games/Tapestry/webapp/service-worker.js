ab211d8392eaba11e6b9d2a09caf8ee7373151fa
sqdog <squaredogstudios@gmail.com>
2021-06-02 10:17:32 -0700

    Tapestry 21

--- games/Tapestry/webapp/service-worker.js
+++ games/Tapestry/webapp/service-worker.js
@@ -1,5 +1,5 @@
 // Service-work cache name
-var cacheName = '2021_06_02__10_10:static';
+var cacheName = '2021_06_02__10_17:static';
 
 // Cache assets on install
 self.addEventListener('install', function(e) {
